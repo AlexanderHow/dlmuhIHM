afdc0b4a6d12373f7b4fed5e4ed2e3625d58152d
Alexandre Howard <alexandre.howard06@gmail.com>
2018-02-05 16:15:47 +0100

    visualisation version admin d'une tache

---
+++ src/main/java/fr/polytech/ihm/Model/Task.java
@@ -0,0 +1,112 @@
+package fr.polytech.ihm.Model;
+
+import javafx.beans.property.*;
+
+public class Task {
+    private StringProperty title;
+    private StringProperty author;
+    private StringProperty assignee;
+    private StringProperty category;
+    private StringProperty date; //TODO : date type en javaFX
+    private StringProperty location;
+    private StringProperty description;
+    private IntegerProperty upvote;
+    private IntegerProperty emergencyLvl; //1(easy) to 3(important)
+    private BooleanProperty resolved = new SimpleBooleanProperty(false);
+
+    public Task(String title,String author,String assignee,String category,String date,String location,String description,int upvote, int emergency){
+        this.title=new SimpleStringProperty(title);
+        this.author=new SimpleStringProperty(author);
+        this.assignee=new SimpleStringProperty(assignee);
+        this.category=new SimpleStringProperty(category);
+        this.date=new SimpleStringProperty(date);
+        this.location=new SimpleStringProperty(location);
+        this.description=new SimpleStringProperty(description);
+        this.upvote=new SimpleIntegerProperty(upvote);
+        this.emergencyLvl=new SimpleIntegerProperty(emergency);
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public StringProperty titleProperty() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author.get();
+    }
+
+    public StringProperty authorProperty() {
+        return author;
+    }
+
+    public String getAssignee() {
+        return assignee.get();
+    }
+
+    public StringProperty assigneeProperty() {
+        return assignee;
+    }
+
+    public String getCategory() {
+        return category.get();
+    }
+
+    public StringProperty categoryProperty() {
+        return category;
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public StringProperty dateProperty() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location.get();
+    }
+
+    public StringProperty locationProperty() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    public int getUpvote() {
+        return upvote.get();
+    }
+
+    public IntegerProperty upvoteProperty() {
+        return upvote;
+    }
+
+    public int getEmergencyLvl() {
+        return emergencyLvl.get();
+    }
+
+    public IntegerProperty emergencyLvlProperty() {
+        return emergencyLvl;
+    }
+
+    public boolean isResolved() {
+        return resolved.get();
+    }
+
+    public BooleanProperty resolvedProperty() {
+        return resolved;
+    }
+
+    public void setResolved(boolean resolved) {
+        this.resolved.set(resolved);
+    }
+}
---
+++ src/main/java/fr/polytech/ihm/VisualiserController.java
@@ -0,0 +1,45 @@
+package fr.polytech.ihm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class VisualiserController {
+    @FXML
+    private Text titleVisuAdmin;
+
+    @FXML
+    private Text authorVisuAdmin;
+
+    @FXML
+    private Text assigneeVisuAdmin;
+
+    @FXML
+    private Text categoryVisuAdmin;
+
+    @FXML
+    private Text dateVisuAdmin;
+
+    @FXML
+    private Text locationVisuAdmin;
+
+    @FXML
+    private TextArea descriptionVisuAdmin;
+
+    @FXML
+    private Button upvoteVisuAdmin;
+
+    @FXML
+    private Text displayUpvoteVisuAdmin;
+
+    @FXML
+    private ImageView imgEmergencyVisuAdmin;
+
+    @FXML
+    private Button returnVisuAdmin;
+
+    @FXML
+    private Button resolvedVisuAdmin;
+}
