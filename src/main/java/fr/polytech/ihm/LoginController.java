a4e354ce717ba93c52c000aaf5bb7b97994598af
Unknown <quentin.moreau@etu.unice.fr>
2018-02-12 14:40:18 +0100

    onClick LoginScreen

--- src/main/java/fr/polytech/ihm/LoginController.java
+++ src/main/java/fr/polytech/ihm/LoginController.java
@@ -1,9 +1,14 @@
 package fr.polytech.ihm;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -16,4 +21,25 @@ public class LoginController {
     @FXML
     private Button login;
 
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void onClick(MouseEvent mouseEvent) throws Exception {
+        String fxmlFile = "/fxml/hello.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            Stage stage = (Stage) login.getScene().getWindow();
+            Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+            Scene scene = new Scene(rootNode, 400, 200);
+            scene.getStylesheets().add("/styles/styles.css");
+
+            stage.setTitle("Hello JavaFX and Maven");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
