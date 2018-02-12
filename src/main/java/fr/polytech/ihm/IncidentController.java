185b0afeb388f5114f11599e23da5fa872f17693
Eric <eric.ung@etu.unice.fr>
2018-02-12 14:57:21 +0100

    Incident fxml and controller added

---
+++ src/main/java/fr/polytech/ihm/IncidentController.java
@@ -0,0 +1,140 @@
+package fr.polytech.ihm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class IncidentController {
+
+    @FXML
+    private ComboBox<?> categoryIncident;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    private Label incidentTitle;
+
+    @FXML
+    private Label incidentLocalisation;
+
+    @FXML
+    private Label incidentDate;
+
+    @FXML
+    private Label incidentDescription;
+
+    @FXML
+    private Label importanceIncident;
+
+    @FXML
+    private Label incidentScore;
+
+    @FXML
+    private Button upvoteIncidentButton;
+
+    @FXML
+    private Label incidentTitle1;
+
+    @FXML
+    private Label incidentLocalisation1;
+
+    @FXML
+    private Label incidentDate1;
+
+    @FXML
+    private Label incidentDescription1;
+
+    @FXML
+    private Label importanceIncident1;
+
+    @FXML
+    private Label incidentScore1;
+
+    @FXML
+    private Button upvoteIncidentButton1;
+
+    @FXML
+    private Label incidentTitle2;
+
+    @FXML
+    private Label incidentLocalisation2;
+
+    @FXML
+    private Label incidentDate2;
+
+    @FXML
+    private Label incidentDescription2;
+
+    @FXML
+    private Label importanceIncident2;
+
+    @FXML
+    private Label incidentScore2;
+
+    @FXML
+    private Button upvoteIncidentButton2;
+
+    @FXML
+    private Label incidentTitle3;
+
+    @FXML
+    private Label incidentLocalisation3;
+
+    @FXML
+    private Label incidentDate3;
+
+    @FXML
+    private Label incidentDescription3;
+
+    @FXML
+    private Label importanceIncident3;
+
+    @FXML
+    private Label incidentScore3;
+
+    @FXML
+    private Button upvoteIncidentButton3;
+
+    @FXML
+    private Label incidentTitle4;
+
+    @FXML
+    private Label incidentLocalisation4;
+
+    @FXML
+    private Label incidentDate4;
+
+    @FXML
+    private Label incidentDescription4;
+
+    @FXML
+    private Label importanceIncident4;
+
+    @FXML
+    private Label incidentScore4;
+
+    @FXML
+    private Button upvoteIncidentButton4;
+
+    @FXML
+    private Button addIncidentButton;
+
+    @FXML
+    void accessUserData(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addIncident(ActionEvent event) {
+
+    }
+
+    @FXML
+    void upvote(ActionEvent event) {
+
+    }
+
+}
